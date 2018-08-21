fe43314cefd0e46a8959f5ea0e59c1ea5fcaeeab
Jo Hooton <joanneelizabethhooton@gmail.com>
2018-08-21 21:49:24 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,7 +1,21 @@
 katz_deli = []
 
-def line
-  if line < 0
-    return "The line is currently empty."
+def take_a_number(line, name)
+  line.push(name)
+  position = line.index(name)
+  puts "Welcome, #{name}. You are number #{line.index(name)+1} in the line."
+  return name, position
+end
+
+def line(line)
+  if line.size == 0
+    puts "The line is currently empty."
+  else
+    message = "The line is currently:"
+
+  line.each_with_index do | position, index |
+    message += "#{index.to_i+1}. #{position}"
   end
+  puts "#{message}"
+end
 end
